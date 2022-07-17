de295770eaeccda73e6d0eda1da3cbb5e58dafbb
Shawn D'souza <shawndsouza@outlook.com>
2022-07-17 20:44:33 +1000

    render grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+import cn from 'classnames';
+
 import './App.css';
 
+const numberOfRows = 6;
+const numberOfColumns = 6;
+
+const generateGrid = () =>
+  Array.from({ length: numberOfRows }, () =>
+    Array.from({ length: numberOfColumns }, () => 0)
+  );
+
 function App() {
-  return <div className="App">Hello world</div>;
+  const [grid] = useState(generateGrid);
+
+  return (
+    <>
+      {grid.map((row, rowIndex) => {
+        return (
+          <div className="row">
+            {row.map((_, columnIndex) => {
+              return (
+                <div
+                  className={cn('cell flex', {
+                    alive: grid[rowIndex][columnIndex] === 1,
+                  })}
+                  key={`${rowIndex}-${columnIndex}`}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default App;
